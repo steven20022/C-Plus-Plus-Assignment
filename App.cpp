5d0b10c620e7bd5f8e2c19c12c0b465ba7741769
steven20022 <jacobelam11@gmail.com>
2022-11-17 09:34:26 -0500

    First init after clone

---
+++ App.cpp
@@ -0,0 +1,50 @@
+int main() {
+    // create a Statistician object named s1 using its one-arg 
+    // constructor giving it an argument of 3.5
+    
+
+    // declare local variables that will be used when calling 
+    // s1's getter method
+    
+
+    // call method on s1 that will display it to stdout
+    
+
+    // call method on s1 that will add 5.2 to it
+    
+
+    // call method on s1 that will display it to stdout
+    
+
+    // call method on s1 that will add -7.8 to it
+    
+
+    // call method on s1 that will display it to stdout
+    
+
+    // call method on s1 that will get its field values
+    // and store them in local variables
+    
+
+    // display values in local variables to stdout
+    
+
+    // create a Statistician object named s2 using its no-arg
+    // constructor
+    
+
+    // call method on s2 that will display it to stdout
+    
+
+    // call method on s1 that will test if it is equal to s2
+    // and display its return to stdout as false or true
+    
+
+    // call method on s2 that will add the double numbers to it 
+    // that will cause it to be equal to s1
+    
+
+    // call method on s1 that will test if it is equal to s2
+    // and display its return to stdout as false or true
+    
+}
